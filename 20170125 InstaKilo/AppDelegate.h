376cc56f7add6e7108a85b2de992b972a9e68970
mlinglhl <minhung.ling@gmail.com>
2017-01-25 13:10:37 -0500

    Initial Commit

---
+++ 20170125 InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20170125 InstaKilo
+//
+//  Created by Minhung Ling on 2017-01-25.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20170125 InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20170125 InstaKilo
+//
+//  Created by Minhung Ling on 2017-01-25.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
